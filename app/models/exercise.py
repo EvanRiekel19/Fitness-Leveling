38fd1f14341465d38e490ea127c2b0ba0fa7a919
EvanRiekel19 <evanriekel19@gmail.com>
2025-04-07 22:46:08 -0400

    Remove duplicate ExerciseSet class from exercise.py

--- app/models/exercise.py
+++ app/models/exercise.py
@@ -9,14 +9,4 @@ class Exercise(db.Model):
     def __repr__(self):
         return f'<Exercise {self.name}>'
 
-
-class ExerciseSet(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    exercise_id = db.Column(db.Integer, db.ForeignKey('exercise.id'), nullable=False)
-    set_number = db.Column(db.Integer, nullable=False)  # e.g., 1, 2, 3 for set 1, set 2, set 3
-    reps = db.Column(db.Integer)  # Number of repetitions
-    weight = db.Column(db.Float)  # Weight in kg
-    notes = db.Column(db.String(200))  # Optional notes for the set
-
-    def __repr__(self):
-        return f'<Set {self.set_number}: {self.reps} reps at {self.weight} kg>' 
+# Removed duplicate ExerciseSet class 
