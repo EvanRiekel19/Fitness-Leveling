20b6a5fbfd17191b6cfe71de25aa6c42b04e413a
EvanRiekel19 <evanriekel19@gmail.com>
2025-04-07 22:26:56 -0400

    Add missing exercise_set model

---
+++ app/models/exercise_set.py
@@ -0,0 +1,16 @@
+from app import db
+
+class ExerciseSet(db.Model):
+    __tablename__ = 'exercise_set'
+
+    id = db.Column(db.Integer, primary_key=True)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercise.id'), nullable=False)
+    set_number = db.Column(db.Integer, nullable=False)
+    reps = db.Column(db.Integer)
+    weight = db.Column(db.Float)
+    notes = db.Column(db.String(500))
+
+    exercise = db.relationship('Exercise', backref=db.backref('sets', lazy=True))
+
+    def __repr__(self):
+        return f'<ExerciseSet {self.set_number} for Exercise {self.exercise_id}>' 
